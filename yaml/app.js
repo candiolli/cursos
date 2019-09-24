745545d77b7e57516439f605a56aa3f4223b8a91
silascandiolli <silascandiolli@cwi.com.br>
2019-09-24 16:51:41 -0300

    Candiolli*123

--- yaml/app.js
+++ yaml/app.js
@@ -4,6 +4,7 @@ const YAML = require('yaml')
 const file = fs.readFileSync('./file1.yaml', 'utf8')
 
 const doc = YAML.parse(file);
+
 console.log(doc.include_newlines)
 console.log("--------------------")
 console.log(doc.fold_newlines)
